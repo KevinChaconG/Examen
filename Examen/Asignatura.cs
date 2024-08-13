5c569878a48dac1d55be70c8245d1f6778d8b41c
Kevin Chacon <kevin27chacon@gmail.com>
2024-08-12 20:30:54 -0600

    creacion de clases e interfaces

---
+++ Examen/Asignatura.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Examen
+{
+    public class Asignatura : Alumno, IAsignatura
+    {
+        public int N1;
+
+        public int N2;
+
+        public int N3;
+        public string NombreAsignatura {  get; set; }
+        public string Horario { get; set; }
+        public string NombreDocente { get; set; }
+
+
+    }
+}
---
+++ Examen/IAsignatura.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Examen
+{
+    public interface IAsignatura
+    {
+        public double CalcularNotaFinal()
+        {
+            return 0;
+        }
+
+        public double CalcularNotaFinal(double n1, double n2, double n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public string MensajeNotaFinal()
+        {
+            Console.WriteLine();
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("---Nota Final---");
+            Console.WriteLine("---Nota Final---");
+        }
+    }
+}
